4ff41cba3880819bd074be0b3094129141de33e1
jfmarkan <jfmarkan@gmail.com>
2023-05-18 16:15:59 +0200

    JS4 - Done! with charAt()

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -3,11 +3,19 @@
 //e calcola la somma di tutte le cifre che compongono il numero.
 
 let fourFigureNumber = prompt('Please enter a 4 figure number');
-let separateFigures = fourFigureNumber.split('');
+// let separateFigures = fourFigureNumber.split('');
 let result = 0;
 
-for ( let i = 0; i < separateFigures.length; i++){
-    result += parseInt(separateFigures[i]); 
+// for ( let i = 0; i < separateFigures.length; i++){
+//     result += parseInt(separateFigures[i]); 
+// }
+
+// console.log(result);
+
+
+for ( let i = 0; i < fourFigureNumber.length; i++){
+    let number = parseInt(fourFigureNumber.charAt(i));
+    result += number; 
 }
 
 console.log(result);
