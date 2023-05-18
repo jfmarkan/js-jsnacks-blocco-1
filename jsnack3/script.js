dd5fe424908a949538bc5dfa72c6b094c771501b
jfmarkan <jfmarkan@gmail.com>
2023-05-18 15:27:02 +0200

    JS3 - Done!

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,14 @@
 //Snack 3:
 //Crea un array vuoto.
 //Chiedi per 6 volte all’utente di inserire un numero,  se è dispari inseriscilo nell’array.
+
+let oddNumbersOnly = [];
+
+for (let i = 0; i < 6; i++){
+    let numbersTyped = prompt('please insert a number');
+    if (numbersTyped % 2 !== 0){
+        console.log('Odd Number');
+        oddNumbersOnly.push(numbersTyped);
+    }
+}
+console.log(oddNumbersOnly);
