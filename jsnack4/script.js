2f9527c095f6ab2fd791216b234cdbb9745cfd5f
jfmarkan <jfmarkan@gmail.com>
2023-05-18 16:07:45 +0200

    JS4 - Done! with .split()

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,3 +1,13 @@
 //Snack 4:
 //Chiedi un numero di 4 cifre all’utente
 //e calcola la somma di tutte le cifre che compongono il numero.
+
+let fourFigureNumber = prompt('Please enter a 4 figure number');
+let separateFigures = fourFigureNumber.split('');
+let result = 0;
+
+for ( let i = 0; i < separateFigures.length; i++){
+    result += parseInt(separateFigures[i]); 
+}
+
+console.log(result);
